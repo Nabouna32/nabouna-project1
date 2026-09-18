e4e285f224691bda890a911496e1bd6386fabe18
Rupy <nabouna@gmx.fr>
2026-09-18 21:38:28 +0200

    fix: remove settings route typo [skip vercel]

--- src/app/[locale]/parametres/page.tsx
+++ src/app/[locale]/parametres/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { getMessages, isLocale, locales, type Locale } from "@/lib/i18n/config";
-c
+
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
