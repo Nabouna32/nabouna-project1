6ca9a345e06df0fefd60cd321aa3bb035b799309
Rupy <nabouna@gmx.fr>
2026-09-18 20:44:34 +0200

    feat: start locale-aware interface [skip vercel]

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,16 @@
 import Categories from "@/components/home/Categories";
 import Hero from "@/components/home/Hero";
 
-export default function HomePage() {
+export default async function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
   return (
     <main>
-      <Hero />
-      <Categories />
+      <Hero locale={locale} />
+      <Categories locale={locale} />
     </main>
   );
 }
