aa08b38cda2b240450be4aa546ab149f01f6d3f6
Rupy <nabouna@gmx.fr>
2026-09-25 04:00:46 +0200

    fix: place primary tool as page content

--- src/app/[locale]/outils/calculs/tva/page.tsx
+++ src/app/[locale]/outils/calculs/tva/page.tsx
@@ -19,7 +19,6 @@ export default async function TVAPage({ params }: { params: Promise<{ locale: st
     <ToolPage
       locale={locale}
       toolId="tva"
-      <TVACalculator />
       content={
         <>
           <div className="space-y-12">{getToolEditorial("tva", locale)}</div>
@@ -27,6 +26,8 @@ export default async function TVAPage({ params }: { params: Promise<{ locale: st
           <RelatedTools toolId="tva" />
         </>
       }
-    />
+    >
+      <TVACalculator />
+    </ToolPage>
   );
 }
